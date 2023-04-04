b62bf8e31cb52e10f3cd110b4f478dd9f73708f2
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-04-04 19:29:11 -0300

    Teste de cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 import swaggerUi from 'swagger-ui-express'
 
 import routes from './routes/router';
 import swaggerDocs from './swagger.json'
 
 const app = express();
+
 app.use(express.json())
-app.use(cors())
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(routes);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 require('dotenv').config();
+import cors from 'cors';
 import app from './app';
 
+app.use(cors())
 app.listen(process.env.PORT);
