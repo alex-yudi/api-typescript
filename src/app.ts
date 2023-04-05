f9e90721fc27b5f7ef97ee35b98f6878894712a5
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-04-05 15:23:58 -0300

    conserto de erro de cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express'
 
 import routes from './routes/router';
@@ -6,8 +7,8 @@ import swaggerDocs from './swagger.json'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(routes);
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 require('dotenv').config();
-import cors from 'cors';
 import app from './app';
 
-app.use(cors())
 app.listen(process.env.PORT);
